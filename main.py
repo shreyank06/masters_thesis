9c63000faf29f3bc585da8961500748f6ced9bfc
Shreyank Saparia <shreyank.saparia@fokus.fraunhofer.de>
2024-01-11 08:56:25 +0100

    trained and saved the model

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ import os
 import pandas as pd
 from datetime import datetime, timedelta
 from merge import load_configuration, fetch_and_convert_data, merge_dataframes
-from model import createXY
+from model import createXY, build_model, retrain_model
 import sys
-
-# import numpy as np
-# from matplotlib import pyplot as plt
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import LSTM
-# from tensorflow.keras.layers import Dense, Dropout
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import LSTM, Dense, Dropout
 from sklearn.preprocessing import MinMaxScaler
-# from keras.wrappers.scikit_learn import KerasRegressor
-# from sklearn.model_selection import GridSearchCV
 
 def main():
     config = load_configuration()
@@ -27,11 +24,11 @@ def main():
         end_time = config['start_time'] + timedelta(seconds=60)
 
         # Fetch and convert all data
-        http_df = fetch_and_convert_data(config, 'http', config['start_time'], end_time, config['step'], column_prefix='http_client_request_count')
-        cpu_df = fetch_and_convert_data(config, 'cpu', config['start_time'], end_time, config['step'], column_prefix='cpu')
+        http_df = fetch_and_convert_data(config, 'http', config['start_time'], end_time, config['step'], column_prefix='http_client_request_count').apply(pd.to_numeric, errors='coerce')
+        cpu_df = fetch_and_convert_data(config, 'cpu', config['start_time'], end_time, config['step'], column_prefix='cpu').apply(pd.to_numeric, errors='coerce')
         allocated_df = fetch_and_convert_data(config, 'allocated', config['start_time'], end_time, config['step'], column_prefix='allocated').apply(pd.to_numeric, errors='coerce')
         wasted_df = fetch_and_convert_data(config, 'wasted', config['start_time'], end_time, config['step'], column_prefix='wasted').apply(pd.to_numeric, errors='coerce')
-        subscriber_count_df = fetch_and_convert_data(config, 'subscriber_count', config['start_time'], end_time, config['step'], column_prefix='subscriber_count')
+        subscriber_count_df = fetch_and_convert_data(config, 'subscriber_count', config['start_time'], end_time, config['step'], column_prefix='subscriber_count').apply(pd.to_numeric, errors='coerce')
         
         result_df = pd.DataFrame()  # Create an empty DataFrame to store the results
 
@@ -42,43 +39,41 @@ def main():
 
         result_df = result_df.filter(like='_used_amf')
 
-
-        # Merge HTTP, CPU, Allocated, Wasted and subscriber dataframes
-        merged_df = merge_dataframes([http_df, cpu_df, subscriber_count_df, result_df])
-        merged_df = merged_df.dropna()
+        # Merge HTTP, CPU, Allocated, Wasted, and subscriber dataframes
+        merged_df = merge_dataframes([http_df, cpu_df, subscriber_count_df, result_df]).dropna()
         timestamps = merged_df.index
-        scaled_features = scaler.fit_transform(merged_df)
-        scaled_df = pd.DataFrame(scaled_features, columns=merged_df.columns, index=timestamps)
-        scaled_df.to_csv(csv_file, index=True, header=True if not os.path.exists(csv_file) else False, mode='a' if os.path.exists(csv_file) else 'w')
-
-        print(scaled_df.shape)
+        scaled_df = pd.DataFrame(scaler.fit_transform(merged_df), columns=merged_df.columns, index=timestamps)
+        #scaled_df.to_csv(csv_file, index=True, header=True if not os.path.exists(csv_file) else False, mode='a' if os.path.exists(csv_file) else 'w')
 
         # Calculate the split index
         split_index = int(len(merged_df) * train_percentage)
 
         # Split the data into training and testing sets
-        train_data = scaled_df.iloc[:split_index]
-        test_data = scaled_df.iloc[split_index:]
+        train_data, test_data = scaled_df.iloc[:split_index], scaled_df.iloc[split_index:]
 
-        trainX,trainY=createXY(train_data,5)
-        testX,testY=createXY(test_data,5)
-
-        print(trainX.shape, trainY.shape)
-        print(trainX[0])
-        print(trainY[0])
-        # # Convert trainX and trainY to DataFrames
-        # trainX_df = pd.DataFrame(trainX.reshape(trainX.shape[0], -1), columns=[f'feature_{i}' for i in range(trainX.shape[1] * trainX.shape[2])])
-        # trainY_df = pd.DataFrame(trainY, columns=['target'])
+        trainX, trainY = createXY(train_data, 5)
+        testX, testY = createXY(test_data, 5)
         
-        # # Concatenate trainX and trainY horizontally
-        # train_combined_df = pd.concat([trainX_df, trainY_df], axis=1)
+        print(f"Training model for time range: {config['start_time']} to {end_time}")
 
-        # # Save combined trainX and trainY to a single CSV file
-        # train_combined_df.to_csv('train_data.csv', index=False)
+        if(config["retrain_model"]):
+            my_model = retrain_model(trainX, trainY, testX, testY)
+            my_model.save("saved_model.h5")
 
-        #sys.exit()
-        #print(trainX, trainY)
+        prediction = load_model("saved_model.h5").predict(testX)
+        prediction_copies_array = np.repeat(prediction, 11, axis=-1)
+        subscriber_count_predicted_value = scaler.inverse_transform(np.reshape(prediction_copies_array, (len(prediction), 11)))[:, 3]
 
+        #Print the reshaped and inverse-transformed predicted values
+        print("Predicted Values -- ", subscriber_count_predicted_value)
+
+        # Continue with the rest of the code
+        original_copies_array = np.repeat(testY, 11, axis=-1)
+        original = scaler.inverse_transform(np.reshape(original_copies_array, (len(testY), 11)))[:, 3]
+
+        # Print the original values
+        print("\nOriginal Values -- ", original)
+        sys.exit()
         config['start_time'] = end_time
         time.sleep(1)  # Sleep for 60 seconds before the next iteration
 
--- model.py
+++ model.py
@@ -1,31 +1,35 @@
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import LSTM, Dense, Dropout
 from sklearn.preprocessing import MinMaxScaler
 from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.model_selection import GridSearchCV
+import numpy as np
 
 def createXY(dataset, n_past):
     dataX = []
     dataY = []
     for i in range(n_past, len(dataset)):
-        dataX.append(dataset.iloc[i - n_past:i, :].values)
+        dataX.append(dataset.iloc[i - n_past:i, 0:].values)
         dataY.append(dataset.iloc[i, 3])
     return np.array(dataX), np.array(dataY)
 
-
 def build_model(optimizer):
     grid_model = Sequential()
-    grid_model.add(LSTM(50,return_sequences=True,input_shape=(30,5)))
+    grid_model.add(LSTM(50,return_sequences=True,input_shape=(5, 11)))
     grid_model.add(LSTM(50))
     grid_model.add(Dropout(0.2))
-    grid_model.add(Dense(1))grid_model.compile(loss = 'mse',optimizer = optimizer)
-    return grid_modelgrid_model = KerasRegressor(build_fn=build_model,verbose=1,validation_data=(testX,testY))
-parameters = {'batch_size' : [16,20],
-              'epochs' : [8,10],
-              'optimizer' : ['adam','Adadelta'] }grid_search  = GridSearchCV(estimator = grid_model,
-                            param_grid = parameters,
-                            cv = 2)
+    grid_model.add(Dense(1))
+    grid_model.compile(loss = 'mse',optimizer = optimizer)
+    return grid_model
+
+def retrain_model(trainX, trainY, testX, testY):
+    grid_model = KerasRegressor(build_fn=build_model, verbose=1, validation_data=(testX, testY))
+    parameters = {'batch_size': [16, 20],
+                  'epochs': [8, 10],
+                  'optimizer': ['adam', 'Adadelta']}
+
+    grid_search = GridSearchCV(estimator=grid_model, param_grid=parameters, cv=2)
+    grid_search = grid_search.fit(trainX, trainY)
+    print("Grid search completed.")
+    my_model = grid_search.best_estimator_.model
+    return my_model

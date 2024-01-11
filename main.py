d996d11f0e4c3162025b73efa21f7d9ca16ecd08
Shreyank Saparia <shreyank.saparia@fokus.fraunhofer.de>
2024-01-11 10:22:50 +0100

    added readme

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ def main():
     column_types = ['global', 'packet', 'session', 'transaction']
     scaler = MinMaxScaler(feature_range=(0,1))
     csv_file = "scaled_merged_http_cpu_mem_data.csv"
-
+      # Create an empty DataFrame to store the results
     while True:
         end_time = config['start_time'] + timedelta(seconds=60)
+        result_df = pd.DataFrame()
 
         # Fetch and convert all data
         http_df = fetch_and_convert_data(config, 'http', config['start_time'], end_time, config['step'], column_prefix='http_client_request_count').apply(pd.to_numeric, errors='coerce')
@@ -29,8 +30,6 @@ def main():
         allocated_df = fetch_and_convert_data(config, 'allocated', config['start_time'], end_time, config['step'], column_prefix='allocated').apply(pd.to_numeric, errors='coerce')
         wasted_df = fetch_and_convert_data(config, 'wasted', config['start_time'], end_time, config['step'], column_prefix='wasted').apply(pd.to_numeric, errors='coerce')
         subscriber_count_df = fetch_and_convert_data(config, 'subscriber_count', config['start_time'], end_time, config['step'], column_prefix='subscriber_count').apply(pd.to_numeric, errors='coerce')
-        
-        result_df = pd.DataFrame()  # Create an empty DataFrame to store the results
 
         for column_type in column_types:
             allocated_col = f'phoenix_memory_cm_{column_type}P_allocated_{component}'
@@ -40,40 +39,38 @@ def main():
         result_df = result_df.filter(like='_used_amf')
 
         # Merge HTTP, CPU, Allocated, Wasted, and subscriber dataframes
-        merged_df = merge_dataframes([http_df, cpu_df, subscriber_count_df, result_df]).dropna()
+        merged_df = merge_dataframes([http_df, cpu_df, subscriber_count_df, result_df])#.dropna()
         timestamps = merged_df.index
         scaled_df = pd.DataFrame(scaler.fit_transform(merged_df), columns=merged_df.columns, index=timestamps)
-        #scaled_df.to_csv(csv_file, index=True, header=True if not os.path.exists(csv_file) else False, mode='a' if os.path.exists(csv_file) else 'w')
-
+        merged_df.to_csv(csv_file, index=True, header=True if not os.path.exists(csv_file) else False, mode='a' if os.path.exists(csv_file) else 'w')
         # Calculate the split index
         split_index = int(len(merged_df) * train_percentage)
 
         # Split the data into training and testing sets
         train_data, test_data = scaled_df.iloc[:split_index], scaled_df.iloc[split_index:]
-
-        trainX, trainY = createXY(train_data, 5)
-        testX, testY = createXY(test_data, 5)
+        trainX, trainY, testX, testY = createXY(train_data, 5) + createXY(test_data, 5)
         
-        print(f"Training model for time range: {config['start_time']} to {end_time}")
+        print(f"Predicting for time range: {config['start_time']} to {end_time}")
 
         if(config["retrain_model"]):
-            my_model = retrain_model(trainX, trainY, testX, testY)
-            my_model.save("saved_model.h5")
+            retrain_model(trainX, trainY, testX, testY).save("saved_model.h5")
+        try:
+            prediction = load_model("saved_model.h5").predict(testX)
+            # Rest of your code for processing predictions
+        except Exception as e:
+            print(f"Error during prediction: {e}")
+            
+        subscriber_count_predicted_value = scaler.inverse_transform(np.reshape(np.repeat(prediction, 11, axis=-1), (len(prediction), 11)))[:, 3]
 
-        prediction = load_model("saved_model.h5").predict(testX)
-        prediction_copies_array = np.repeat(prediction, 11, axis=-1)
-        subscriber_count_predicted_value = scaler.inverse_transform(np.reshape(prediction_copies_array, (len(prediction), 11)))[:, 3]
+        # Print the reshaped and inverse-transformed predicted values
+        print("Predicted subsriber count -- ", subscriber_count_predicted_value)
 
-        #Print the reshaped and inverse-transformed predicted values
-        print("Predicted Values -- ", subscriber_count_predicted_value)
-
-        # Continue with the rest of the code
-        original_copies_array = np.repeat(testY, 11, axis=-1)
-        original = scaler.inverse_transform(np.reshape(original_copies_array, (len(testY), 11)))[:, 3]
+        # original values 
+        original = scaler.inverse_transform(np.reshape(np.repeat(testY, 11, axis=-1), (len(testY), 11)))[:, 3]
 
         # Print the original values
         print("\nOriginal Values -- ", original)
-        sys.exit()
+        #sys.exit()
         config['start_time'] = end_time
         time.sleep(1)  # Sleep for 60 seconds before the next iteration
 

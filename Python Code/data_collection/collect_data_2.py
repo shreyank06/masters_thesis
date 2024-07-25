b0744a518e151ec383046abc2ba09018545eae44
Shreyank Saparia <shreyank.saparia@fokus.fraunhofer.de>
2024-07-25 11:15:38 +0200

    added folder for raw data

--- Python Code/data_collection/collect_data_2.py
+++ Python Code/data_collection/collect_data_2.py
@@ -35,20 +35,90 @@ class CsvCollector:
         return series_numbers, component_folder_path
         
     def fetch_and_save_csv(self):
-        # Check if 'data' folder exists, if not, create it
-        if not os.path.exists('data'):
-            os.makedirs('data')
+        if not self.config["transform_data"] and self.config["regroup_columns"]:
+            if not os.path.exists('raw_data'):
+                os.makedirs('raw_data')
 
-        csv_filename = f"{self.component}_{self.registration_number}_{self.ops_per_second}_set.csv"
-        
-        counter = 0
-        while os.path.exists(os.path.join('data', csv_filename)):
-            counter += 1
-            csv_filename = f"{self.component}_{self.registration_number}_{self.ops_per_second}_set_{counter}.csv"
+            csv_filename = f"{self.component}_{self.registration_number}_{self.ops_per_second}_set.csv"
+            
+            counter = 0
+            while os.path.exists(os.path.join('raw_data', csv_filename)):
+                counter += 1
+                csv_filename = f"{self.component}_{self.registration_number}_{self.ops_per_second}_set_{counter}.csv"
 
-        # Save original CSV file in 'data' folder
-        self.df.to_csv(os.path.join('data', csv_filename), index=True)
-        
+            # Save original CSV file in 'data' folder
+            self.df.to_csv(os.path.join('raw_data', csv_filename), index=True)
+            print(f"Results CSV file saved as: {csv_filename} in 'raw_data' folder.")
+            
+        else:
+            # Check if 'data' folder exists, if not, create it
+            if not os.path.exists('processed_data'):
+                os.makedirs('processed_data')
+
+            csv_filename = f"{self.component}_{self.registration_number}_{self.ops_per_second}_set.csv"
+            
+            counter = 0
+            while os.path.exists(os.path.join('data', csv_filename)):
+                counter += 1
+                csv_filename = f"{self.component}_{self.registration_number}_{self.ops_per_second}_set_{counter}.csv"
+
+            # Save original CSV file in 'data' folder
+            self.df.to_csv(os.path.join('data', csv_filename), index=True)
+            print(f"Results CSV file saved as: {csv_filename} in 'processed_data' folder.")
+
+
+    def collect_csv_data(self):
+            self.df = fetch_and_convert_data(self.config, 'queries', self.start_time, self.end_time, self.config['step'], self.component)
+            self.fetch_and_save_csv()
+
+            # existing_series_numbers,component_folder_path = self.get_existing_series()
+            # if existing_series_numbers:
+            #     new_series = max(existing_series_numbers) + 1
+            # else:
+            #     new_series = 0
+            # file_suffix = f"_s{new_series}"
+
+            # # csv_file_path = os.path.join(component_folder_path, f"{self.registration_number}_{self.ops_per_second}_{0}{file_suffix}_{self.component}.csv")
+            # # self.df.to_csv(csv_file_path, index=True, header=True if not os.path.exists(csv_file_path) else False, mode='a' if os.path.exists(csv_file_path) else 'w')
+
+            # self.visualize_data(self.df, component_folder_path, file_suffix)
+
+            # sys.exit()
+            # self.start_time = self.end_time
+            # time.sleep(1)  # Sleep for 60 sec
+
+    def visualize_data(self, df, folder_path, file_suffix):
+        index_column = df.index
+
+        num_columns = len(df.columns)
+
+        # Create an empty figure
+        fig = go.Figure()
+
+        # Add traces for each column
+        for col in df.columns[0:]:  # Start from the second column
+            fig.add_trace(go.Scatter(x=index_column, y=df[col], mode='lines', name=col))
+
+        # Update layout
+        fig.update_layout(
+            title=f'All Columns vs {index_column}',
+            xaxis_title=str(index_column),  # Convert DatetimeIndex to string
+            yaxis_title='Values',
+            autosize=False,
+            width=1000,
+            height=600,
+            # Enable interactive legend for column filtering
+            legend=dict(traceorder="normal")
+        )
+
+
+        # Save the plot as an HTML file
+        html_file_path = os.path.join(folder_path, f"{self.registration_number}_{self.ops_per_second}_{0}{file_suffix}_all_columns_vs_index.html")
+        fig.write_html(html_file_path)
+
+        # Close the figure
+        #fig.close()
+
         # # Get the latest UE count information
         # latest_ue_count = self.df['subscriber_count_Connected'].iloc[-1]
 
@@ -219,59 +289,3 @@ class CsvCollector:
         #         # If results CSV file doesn't exist, create a new one
         #         df_results.to_csv(results_csv_path, index=False)
         #         print("Results CSV file created.")
-
-        print(f"Results CSV file saved as: {csv_filename} in 'data' folder.")
-
-
-    def collect_csv_data(self):
-            self.df = fetch_and_convert_data(self.config, 'queries', self.start_time, self.end_time, self.config['step'], self.component)
-            self.fetch_and_save_csv()
-
-            # existing_series_numbers,component_folder_path = self.get_existing_series()
-            # if existing_series_numbers:
-            #     new_series = max(existing_series_numbers) + 1
-            # else:
-            #     new_series = 0
-            # file_suffix = f"_s{new_series}"
-
-            # # csv_file_path = os.path.join(component_folder_path, f"{self.registration_number}_{self.ops_per_second}_{0}{file_suffix}_{self.component}.csv")
-            # # self.df.to_csv(csv_file_path, index=True, header=True if not os.path.exists(csv_file_path) else False, mode='a' if os.path.exists(csv_file_path) else 'w')
-
-            # self.visualize_data(self.df, component_folder_path, file_suffix)
-
-            # sys.exit()
-            # self.start_time = self.end_time
-            # time.sleep(1)  # Sleep for 60 sec
-
-    def visualize_data(self, df, folder_path, file_suffix):
-        index_column = df.index
-
-        num_columns = len(df.columns)
-
-        # Create an empty figure
-        fig = go.Figure()
-
-        # Add traces for each column
-        for col in df.columns[0:]:  # Start from the second column
-            fig.add_trace(go.Scatter(x=index_column, y=df[col], mode='lines', name=col))
-
-        # Update layout
-        fig.update_layout(
-            title=f'All Columns vs {index_column}',
-            xaxis_title=str(index_column),  # Convert DatetimeIndex to string
-            yaxis_title='Values',
-            autosize=False,
-            width=1000,
-            height=600,
-            # Enable interactive legend for column filtering
-            legend=dict(traceorder="normal")
-        )
-
-
-        # Save the plot as an HTML file
-        html_file_path = os.path.join(folder_path, f"{self.registration_number}_{self.ops_per_second}_{0}{file_suffix}_all_columns_vs_index.html")
-        fig.write_html(html_file_path)
-
-        # Close the figure
-        #fig.close()
-
